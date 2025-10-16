b7caef742ca1e80fc2d2469d76a4e1b4a15106d4
Yerubandi Eswara Prasad <eswaraprasad469@gmail.com>
2025-10-16 07:07:11 +0530

    Add routes for Terms of Service and Privacy Policy

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,8 @@ import AboutMe from "./pages/Aboubt";
 import Services from "./pages/Services";
 import Contact from "./pages/Contact";
 import WhatsappButton from "./components/custom/WhatsappButton";
+import TermsOfService from "./pages/terms-of-service";
+import PrivacyPolicy from "./pages/privacy-policy"
 
 const App = () => {
   return (
@@ -14,6 +16,8 @@ const App = () => {
           <Route path="/about" element={<AboutMe />} />
           <Route path="/services" element={<Services />} />
           <Route path="/contact" element={<Contact />} />
+          <Route path="/privacy-policy" element={<PrivacyPolicy />} />
+          <Route path="/terms-of-service" element={<TermsOfService />} />
         </Routes>
         <WhatsappButton />
       </BrowserRouter>
