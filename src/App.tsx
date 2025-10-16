ef4469cf4e4f28bfb048c080a59d01cd9acf1795
eswaraprasad <esweraprasad@gmail.com>
2025-10-16 08:39:19 +0530

    chnages

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import AboutMe from "./pages/Aboubt";
 import Services from "./pages/Services";
 import Contact from "./pages/Contact";
 import WhatsappButton from "./components/custom/WhatsappButton";
-import TermsOfService from "./pages/terms-of-service";
+import TermsOfService from "./pages/erms-of-Service";
 import PrivacyPolicy from "./pages/privacy-policy";
 
 const App = () => {
