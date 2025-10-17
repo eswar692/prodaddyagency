51f36d50a420d798efa7a8d28026526ea170cd60
eswaraprasad <esweraprasad@gmail.com>
2025-10-17 12:29:03 +0530

    changes

--- src/components/common/Footer.tsx
+++ src/components/common/Footer.tsx
@@ -31,17 +31,17 @@ const Footer = () => {
           </nav>
         </div>
         {/* Details */}
-       <div className="mx-auto text-center space-y-2">
-  <h3 className="text-2xl font-bold text-transparent bg-clip-text bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500">
-    Pro Daddy Agency Address
-  </h3>
-  <ul className="text-gray-700 text-lg leading-relaxed">
-    <li>2-148, Edarada, High School Road</li>
-    <li>Tatipaka, Konaseema</li>
-    <li>Andhra Pradesh, 533247</li>
-  </ul>
-</div>
-
+        <div className="mx-auto text-center space-y-2">
+          <h3 className="text-2xl font-bold text-transparent bg-clip-text bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500">
+            Pro Daddy Agency Address
+          </h3>
+          <ul className="text-gray-700 text-lg leading-relaxed">
+            <li>2-148, Edarada, High School Road</li>
+            <li>Tatipaka, Konaseema</li>
+            <li>Andhra Pradesh, 533247</li>
+            <li>93989 55055</li>
+          </ul>
+        </div>
 
         {/* Bottom line */}
         <div className="mt-6 border-t pt-4 text-center text-sm text-gray-500 dark:text-gray-400">
